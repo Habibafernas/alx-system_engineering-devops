a5dd0da2d76bda50be4e5703d5779bf14fe4b49f
Habibafernas <habibafernas@gmail.com>
2024-04-10 11:57:54 -0700

    updating

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" returns a list containing the titles of all hot articles for a given subreddit."""
+"""
+Using reddit's API
+"""
 import requests
 after = None
 
+
 def recurse(subreddit, hot_list=[]):
-    """ returns a list containing the titles of all hot articles for a given subreddit."""
+    """returning top ten post titles recursively"""
     global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    user_agent = {'User_Agent': 'api_advanced-project'}
     parameters = {'after': after}
     results = requests.get(url, params=parameters, headers=user_agent,
                            allow_redirects=False)
@@ -22,4 +25,4 @@ def recurse(subreddit, hot_list=[]):
             hot_list.append(title_.get("data").get("title"))
         return hot_list
     else:
-        return(None)
+        return (None)
