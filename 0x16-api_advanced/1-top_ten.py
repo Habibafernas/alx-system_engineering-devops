b754999ec98fb9b82d63909f485a075e1ed6179c
Habibafernas <habibafernas@gmail.com>
2024-04-10 10:38:19 -0700

    a

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,6 +5,8 @@ import requests
 
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": 'Google Chrome Version 81.0.4044.129'
@@ -13,8 +15,5 @@ def top_ten(subreddit):
         "limit": 10
     }
     response = requests.get(url, headers=headers, params=params)
-    if response.status_code == 404:
-        print("None")
-        return
     results = response.json()
     [print(c.get("data").get("title")) for c in results.get("children")]
