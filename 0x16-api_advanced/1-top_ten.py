ab4c1bb6dcfc2335d6342426af32528855dbf3c7
Habibafernas <habibafernas@gmail.com>
2024-04-10 10:40:43 -0700

    a

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
-import requests
+
+"""
+prints the titles of the first 10 hot posts listed for a given subreddit
+"""
+
+from requests import get
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
+    """
+    function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit
+    """
+
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": 'Google Chrome Version 81.0.4044.129'
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params)
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
     results = response.json()
+
     try:
         my_data = results.get('data').get('children')
 
