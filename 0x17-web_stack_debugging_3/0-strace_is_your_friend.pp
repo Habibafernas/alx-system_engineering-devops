afa271a99bf3534f327f0f0b2f944237c7c32c38
HabibaFernas <habibafernas@gmail.com>
2024-04-11 21:43:30 +0200

    updating

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #this file is to solve the problem of the server
 
-$file = '/var/www/html/wp-settings.php'
+$file = '/var/www/html/wordpress/wp-settings.php'
 
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file}",
