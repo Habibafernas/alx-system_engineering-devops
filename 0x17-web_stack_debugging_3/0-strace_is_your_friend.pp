b230f28643a933552806fc8b80041f61af8fb840
Habibafernas <habibafernas@gmail.com>
2024-04-11 11:57:56 -0700

    updating

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,8 @@
+#this file is to solve the problem of the server
+
+$file = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file}",
+  path => ['/bin', '/usr/bin']
+}
